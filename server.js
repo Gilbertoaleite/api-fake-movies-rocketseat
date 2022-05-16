e155ec7ab01e47391848bb6d1d5c77b60b33332c
GILBERTO DE ARAUJO LEITE <gilbertoaleite@hotmail.com>
2022-05-16 00:08:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken')
 
 const server = jsonServer.create()
-const router = jsonServer.router('./database.json')
+const router = jsonServer.router('db.json')
 const assetpk = jsonServer.assetpk('./database2.json')
 const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
 
